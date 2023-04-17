f38e00155c604687557264c80fae6ad268aec553
jylmarcus <maplecow@gmail.com>
2023-04-17 19:30:22 +0800

    code up to slide 24

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -54,5 +54,54 @@ public class App
                 System.out.println("Seems like you are a boring person without hobby");
                 break;
         }
+
+        //Slide 19
+        Integer myAge = 0;
+        String inputAge = cons.readLine("What is your age?");
+
+        myAge = Integer.parseInt(inputAge);
+
+        if(myAge >=0 && myAge <= 3) {
+            System.out.println("you are baby");
+        } else if(myAge > 2 && myAge <= 4) {
+            System.out.println("you are toddler");
+        } else if (myAge > 4 && myAge <= 12) {
+            System.out.println("you are child");
+        } else if (myAge > 12 && myAge <= 19) {
+            System.out.println("you are teenager");
+        } else {
+            System.out.println("you are old and have back pains");
+        }
+
+        //Slide 21
+        Integer i = 0;
+        input = cons.readLine("Type a phrase: ");
+
+        while(i<input.length()) {
+            System.out.println(input.substring(0, i));
+            i++;
+        }
+
+        for(int idx = 0; idx <= input.length(); idx++) {
+            System.out.println(input.substring(0, i));
+        }
+
+        //Slide 23
+        String[] todo = new String[5];
+        for(int pos = 0; pos < todo.length; pos++) {
+            input = cons.readLine("Enter Todo task " + (pos + 1) + ": ");
+            todo[pos] = input;
+        }
+
+        for (int pos = 0; pos < todo.length; pos++) {
+            System.out.println("Task "+ (pos + 1) + "--> " + todo[pos]);
+        }
+
+        String strName = "NUS";
+        //Slide 24
+        if (args.length >0) {
+            strName = args[0];
+        }
+        System.out.println("Hello " + strName);
     }
 }
