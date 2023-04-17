93cb8a481b87d962ebc29f9fa7a37e27122055d6
jylmarcus <maplecow@gmail.com>
2023-04-17 14:09:12 +0800

    added command to read and print console input

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.iss;
 
+import java.io.Console;
 /**
  * Hello world!
  *
@@ -9,5 +10,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Console cons = System.console();
+        String name = cons.readLine("What is your name? ");
+        System.out.printf("Hello %s. Pleased to meet you.\n", name);
+
     }
 }
