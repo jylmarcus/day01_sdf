099848c90fe90e494a799c47128fd88324d2d7c4
jylmarcus <maplecow@gmail.com>
2023-04-17 14:57:03 +0800

    updated to slide 18

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -13,7 +13,46 @@ public class App
 
         Console cons = System.console();
         String name = cons.readLine("What is your name? ");
-        System.out.printf("Hello %s. Pleased to meet you.\n", name);
 
+        if(name.length()>0){
+            System.out.printf("Nice to meet you, %s\n", name);
+        } else {
+            System.err.println("You have not told me your name.");
+        }
+
+        Integer age = 80;
+        System.out.println("\n" + name + " is " + age + " years old");
+
+        //Slide 17
+        //read input from keyboard
+        String input = cons.readLine("What is your hobby?");
+        input.trim(); //remove whitespace from front and end of input
+
+        if(input.equals("swim")){
+            System.out.println("The nearest swimming pools is Clementi");
+        } else if (input.equals("jog")){
+            System.out.println("The nearest park is West Coast park");
+        } else if (input.equals("cycle")){
+            System.out.println("You could cycle along the PCN");
+        } else {
+            System.out.println("Seems like you are a boring person without hobby");
+        }
+
+        //Slide 18
+        //switch statement
+        switch(input) {
+            case "swim":
+                System.out.println("The nearest swimming pools is Clementi");
+                break;
+            case "jog":
+                System.out.println("The nearest park is West Coast park");
+                break;
+            case "cycle":
+                System.out.println("You could cycle along the PCN");
+                break;
+            default:
+                System.out.println("Seems like you are a boring person without hobby");
+                break;
+        }
     }
 }
